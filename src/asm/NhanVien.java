7bb9124f47a06f2967cc5db8b5956d5e07fdd901
CHINHHTPH46334 <chinhhtph46334@fpt.edu.vn>
2023-10-05 13:04:16 +0700

    hehe

--- src/asm/NhanVien.java
+++ src/asm/NhanVien.java
@@ -2,13 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package asm_chinhhtph46334;
+package asm;
 
 /**
  *
  * @author admin
  */
-public class ASM_CHINHHTPH46334 {
+public class NhanVien {
 
     /**
      * @param args the command line arguments
